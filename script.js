0546d23fa90f16bd78f555b180b4e36f5e0776ce
HYDRA-OKAIDA <nicolas.lubawski@escola.pr.gov.br>
2025-09-12 11:00:42 -0300

    ok

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-const avanca=document.query
+const avanca=document.querySelectorAll ('.btn-proximo') ;
+const reiniciarBtn=document.getElementById('.btn-reiniciar');
+
+
+
+avanca.forEach(button => {
+    button.addEventListener ('click', fuction(){
+      const atual=document.querySelector('.ativo');
+      const proximoPasso='passo-'+this.getAttribute('data-proximo');
+      
+      atual.classList.remove('ativo')
+      const proximoElemento=document.getElementById(proximoPasso);
+      if(proximoElemento){
+     proximoElemento.classList.add('ativo');
+      } else{
+     console.error(`Element com ID "${proximoPasso}" não encontrado.`);
+    }
+})
